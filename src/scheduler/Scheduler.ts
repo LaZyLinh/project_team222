3f52e60da8c4cc18d373129c8d868b0a4e69a8ff
LazyLinh <linh.phan3413@gmail.com>
2019-11-14 17:12:18 -0800

    tried listDataSets

--- src/scheduler/Scheduler.ts
+++ src/scheduler/Scheduler.ts
@@ -7,3 +7,18 @@ export default class Scheduler implements IScheduler {
         return [];
     }
 }
+
+/*
+var R = 6371e3; // metres                                       // calculate great-circle distance. need to /1372
+var φ1 = lat1.toRadians();
+var φ2 = lat2.toRadians();
+var Δφ = (lat2-lat1).toRadians();
+var Δλ = (lon2-lon1).toRadians();
+
+var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+var d = R * c;
+ */
