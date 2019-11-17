793a8e296fa1d4d06e363e88c1fc9514af3d7c28
kaliesophic <aiden.smith10@gmail.com>
2019-11-16 19:43:12 -0800

    Beginning query builder, got sender and index, merged changes from D3 Linh

--- frontend/public/query-builder.js
+++ frontend/public/query-builder.js
@@ -1,3 +1,5 @@
+import {ICourse, ImKeyEntry, IsKeyEntry} from "../../src/controller/IDataset";
+
 /**
  * Builds a query object using the current document object model (DOM).
  * Must use the browser's global document object {@link https://developer.mozilla.org/en-US/docs/Web/API/Document}
@@ -5,9 +7,101 @@
  *
  * @returns query object adhering to the query EBNF
  */
-CampusExplorer.buildQuery = function() {
+CampusExplorer.buildQuery = function () {
     let query = {};
-    // TODO: implement!
-    console.log("CampusExplorer.buildQuery not implemented yet.");
+    let activeTabPanel = document.getElementsByClassName("tab-panel active")[0];
+    let id = activeTabPanel.getAttribute("data-type");
+
+    query.WHERE = whereBuilder(id, activeTabPanel.children[0]);
+    query.OPTIONS = optionsBuilder(id, activeTabPanel.children[0]);
+    let TRANSFORMATIONS = transBuilder(id, activeTabPanel.children[0]);
+    if (TRANSFORMATIONS !== null) {
+        query.TRANSFORMATIONS = TRANSFORMATIONS;
+    }
     return query;
 };
+
+// TODO: there's probably a better place for these constants?
+const coursesTypes = [
+    {key: "year",       type: "number"},
+    {key: "avg",        type: "number"},
+    {key: "pass",       type: "number"},
+    {key: "fail",       type: "number"},
+    {key: "audit",      type: "number"},
+    {key: "dept",       type: "string"},
+    {key: "id",         type: "string"},
+    {key: "instructor", type: "string"},
+    {key: "title",      type: "string"},
+    {key: "uuid",       type: "string"},
+];
+
+const roomsTypes = [
+    {key: "fullname",   type: "string"},
+    {key: "shortname",  type: "string"},
+    {key: "number",     type: "string"},
+    {key: "name",       type: "string"},
+    {key: "address",    type: "string"},
+    {key: "lat",        type: "number"},
+    {key: "lon",        type: "number"},
+    {key: "seats",      type: "number"},
+    {key: "type",       type: "string"},
+    {key: "furniture",  type: "string"},
+    {key: "href",       type: "string"},
+];
+
+// return an object of the form {kind_label: value}, with value converted to the relevant type based on the label
+// i.e, if we call this with "courses", "avg", and "95", then return {courses_avg: 95}
+function matchValueType(kind, label, value) {
+    label = label.toLowerCase();
+    let checkArray = roomsTypes;
+    if (kind === "courses"){
+        checkArray = coursesTypes;
+    }
+    let result = {};
+    for (let pair of checkArray) {
+        if (pair.key === label) {
+            if (pair.type === "string") {
+                result[kind + "_" + label] = value;
+                return result;
+            } else {
+                // it's a number
+                result[kind + "_" + label] = Number(value);
+                return result;
+            }
+        }
+    }
+}
+
+function whereBuilder(kind, panel) {
+    let where = {};
+    let conditionType = "";
+    if (panel.getElementsByClassName("control conditions-any-radio")[0].children[0].checked) {
+        conditionType = "any";
+    } else if (panel.getElementsByClassName("control conditions-all-radio")[0].children[0].checked) {
+        conditionType = "all";
+    } else if (panel.getElementsByClassName("control conditions-none-radio")[0].children[0].checked) {
+        conditionType = "none";
+    }
+    let conditions = [];
+    for (cond of activeTabPanel.getElementsByClassName("control-group condition")) {
+        let newCond = {};
+        newCond[cond.children[2].children[0].selectedOptions[0].label] = matchValueType(
+            kind,
+            cond.children[1].children[0].selectedOptions[0].label,
+            cond.children[3].firstElementChild.value);
+
+        conditions.push(newCond);
+    }
+
+    return undefined;
+}
+
+function optionsBuilder(id, panel) {
+    return undefined;
+}
+
+function transBuilder(id, panel) {
+    return undefined;
+}
+
+
--- frontend/public/query-index.js
+++ frontend/public/query-index.js
@@ -8,4 +8,12 @@
  * 4.) Result is rendered in the reference UI by calling CampusExplorer.renderResult with the response from the endpoint as argument
  */
 
-// TODO: implement!
+// TODO: test me!
+
+let submitButton = document.getElementsByClassName("submit btn")[0];
+submitButton.addEventListener("click", (event) => {
+    let query = CampusExplorer.buildQuery();
+    CampusExplorer.sendQuery(query).then((response) => {
+        CampusExplorer.renderResult(response);
+    });
+});
